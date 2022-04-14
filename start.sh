65392f56decfaafe123c58361725c508a0cce12c
wolfrik11 <72972221+wolfrik11@users.noreply.github.com>
2022-04-14 11:26:28 +0300

    Add files via upload

--- start.sh
+++ start.sh
@@ -1,9 +1,1 @@
-@echo off
-IF NOT EXIST BuildTools (
-    mkdir BuildTools
-)
-cd BuildTools
-curl -z BuildTools.jar -o BuildTools.jar https://hub.spigotmc.org/jenkins/job/BuildTools/lastSuccessfulBuild/artifact/target/BuildTools.jar
-set /p Input=Enter the version: || set Input=latest
-java -jar BuildTools.jar --rev %Input%
-pause
+/usr/lib/jvm/jdk-18/bin/java -jar spigot-1.18.2.jar
